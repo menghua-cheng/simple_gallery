7f8404a8c78c1011b3c15ef2eaaf995db8d8f5e7
Kelvin Cheng <Kelvin.Cheng@garmin.com>
2014-05-30 18:27:57 +0800

    Update image utils
    Change-Id: Ia5c12e21d2235ffbd4e6218473f7dffb1d9b02cd

--- utils/gen_indexhtml.sh
+++ utils/gen_indexhtml.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-image_width=`identify out0_image.jpg |cut -d ' ' -f3 |cut -d 'x' -f 1`
-image_height=`identify out0_image.jpg |cut -d ' ' -f3 |cut -d 'x' -f 2`
+img=${1}
+if [ -f ${img} ];then
+    image_width=` identify ${img} |cut -d ' ' -f3 |cut -d 'x' -f 1`
+    image_height=`identify ${img} |cut -d ' ' -f3 |cut -d 'x' -f 2`
+    echo w:${image_width} h:${image_height}
+fi
--- utils/split_image.sh
+++ utils/split_image.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-convert -crop 25%x25% +repage ./IMAG0063_1.jpg out%d_image.jpg
+target_image=${1}
+if [ -f ${target_image} ];then
+    target_path0=`dirname ${target_image}`
+    target_path=`readlink -f ${target_path0}`
+    out_filename_base=`basename ${target_image} .jpg`
+    convert -crop 25%x25% +repage "${target_image}" "${target_path}/${out_filename_base}_%02d.jpg"
+else
+    echo ${target_image} not found
+fi
